b232fcf2142ac35d2fbb813d1468fae20e87237f
Shane Israel <shanemisrael@gmail.com>
2024-06-15 00:07:40 -0600

    service worker cleanup

--- client/src/index.js
+++ client/src/index.js
@@ -2,32 +2,32 @@ import { createRoot } from 'react-dom/client'
 import { NextUIProvider } from '@nextui-org/react'
 import { Toaster } from 'sonner'
 import App from './App'
+import * as serviceWorker from './serviceWorker'
 import './index.css'
 import 'react-lazy-load-image-component/src/effects/blur.css'
 import 'react-lazy-load-image-component/src/effects/opacity.css'
-
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker
-      .register('/worker.js')
-      .then(
-        function (registration) {
-          console.log('Worker registration successful', registration.scope)
-        },
-        function (err) {
-          console.log('Worker registration failed', err)
-        },
-      )
-      .catch(function (err) {
-        console.log(err)
-      })
-  })
-} else {
-  console.log('Service Worker is not supported by browser.')
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function () {
+//     navigator.serviceWorker
+//       .register('/worker.js')
+//       .then(
+//         function (registration) {
+//           console.log('Worker registration successful', registration.scope)
+//         },
+//         function (err) {
+//           console.log('Worker registration failed', err)
+//         },
+//       )
+//       .catch(function (err) {
+//         console.log(err)
+//       })
+//   })
+// } else {
+//   console.log('Service Worker is not supported by browser.')
+// }
 
 root.render(
   <NextUIProvider>
@@ -37,3 +37,5 @@ root.render(
     </main>
   </NextUIProvider>,
 )
+
+serviceWorker.register()
