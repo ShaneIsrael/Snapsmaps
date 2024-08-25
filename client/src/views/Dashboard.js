1698d651dc83f65fbcbeca211635d3f001c7f524
Shane Israel <shanemisrael@gmail.com>
2024-08-25 12:36:14 -0600

    Update Dashboard.js

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -46,7 +46,7 @@ const Dashboard = () => {
       }
       setLastScrollY(window.scrollTop)
 
-      const bottom = Math.round(e.target.scrollHeight - e.target.scrollTop) >== Math.round(e.target.clientHeight - 50)
+      const bottom = Math.round(e.target.scrollHeight - e.target.scrollTop) >= Math.round(e.target.clientHeight - 50)
       console.log(e.target.scrollHeight - e.target.scrollTop, e.target.clientHeight)
       if (bottom) {
         selectedFeed === 'world' ? worldFeed.nextPage() : followingFeed.nextPage()
