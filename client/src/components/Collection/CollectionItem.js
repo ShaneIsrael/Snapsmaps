15659b1654a196e90e1f885e7d1677392e2cd4a6
Shane Israel <shanemisrael@gmail.com>
2024-09-09 23:20:57 -0600

    make collection item just a little bit shorter

--- client/src/components/Collection/CollectionItem.js
+++ client/src/components/Collection/CollectionItem.js
@@ -5,7 +5,7 @@ import clsx from 'clsx'
 function CollectionItem({ collection, onClick }) {
   return (
     <div
-      className="relative rounded-xl border-large border-solid border-neutral-200 h-48 max-w-[478px] w-full bg-gray-900 cursor-pointer"
+      className="relative rounded-xl border-large border-solid border-neutral-200 h-40 max-w-[478px] w-full bg-gray-900 cursor-pointer"
       onClick={onClick}
     >
       <div className="absolute w-full bottom-0 flex items-end text-4xl font-lobster pl-2 pb-2 pt-2 rounded-b-xl bg-black/50">
