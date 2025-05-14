ea4f5a62eeaa67fd07cb43b19863cd63f55c4471
Shane Israel <shanemisrael@gmail.com>
2025-05-14 08:39:35 -0600

    create initial directory paths if they dont exist on start, fix nginx mime types for app serving

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,10 @@ nginx -g 'daemon on;'
 
 node /generateManifest.js
 node /generateMeta.js
+
+mkdir -p /images/post
+mkdir -p /images/profile
+mkdir -p /images/collection
+mkdir -p /images/thumb/120x120
+
 cd /app && npm run migrate && node /app/src/server.js
