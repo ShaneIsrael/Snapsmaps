92972b9dba72cf4d73b28ad87c9c6177fb513bc7
Shane Israel <shanemisrael@gmail.com>
2025-05-14 21:17:45 -0600

    fix script path

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,11 @@ mkdir -p /content/images/profile
 mkdir -p /content/images/collection
 mkdir -p /content/images/thumb/120x120
 
+<<<<<<< Updated upstream
+=======
+node /generateManifest.js
+node /generateMeta.js
+node /app/src/scripts/processImages.js
+
+>>>>>>> Stashed changes
 cd /app && npm run migrate && node /app/src/server.js
